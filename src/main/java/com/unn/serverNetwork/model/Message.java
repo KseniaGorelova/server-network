5c071e4e8031383cd608e62d880eb6655a61f7cb
Pozdeeva <varvarapozdeeva@gmail.com>
2019-12-13 23:06:57 +0300

    change socketMessage

--- src/main/java/com/unn/serverNetwork/model/Message.java
+++ src/main/java/com/unn/serverNetwork/model/Message.java
@@ -1,40 +1,14 @@
 package com.unn.serverNetwork.model;
 
-/*
- * Message received from client.
- *
- * @Author Jay Sridhar
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Message
 {
     private String from;
-    private int text;
-
-    public Message() {}
-
-    public Message(String from, int text)
-    {
-	this.from = from;
-	this.text = text;
-    }
-
-    public String getFrom()
-    {
-        return from;
-    }
-
-    public void setFrom(String from)
-    {
-        this.from = from;
-    }
-
-    public int getText()
-    {
-        return text;
-    }
-
-    public void setText(int text)
-    {
-        this.text = text;
-    }
+    private int status;
+    private String idNeA;
+    private String idNeZ;
 }
